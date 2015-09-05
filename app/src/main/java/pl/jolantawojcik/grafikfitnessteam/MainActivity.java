bdffa81276d9904d56db130d7c18b6fce4986706
JolantaWojcik <jola.j.wk@gmail.com>
2015-09-05 02:46:04 +0200

    MainActivity changes

--- app/src/main/java/pl/jolantawojcik/grafikfitnessteam/MainActivity.java
+++ app/src/main/java/pl/jolantawojcik/grafikfitnessteam/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
@@ -28,20 +29,19 @@ import java.util.regex.Pattern;
 
 public class MainActivity extends Activity {
 
-    private ListView listView;
     ListViewAdapter adapter;
     DBHandler dbHandler;
     Record record;
     private static final String PING_URL = "http://www.fitnessteam.net/grafik_zajec.html";
     private ProgressDialog progressDialog;
     ArrayList<HashMap<String, String>> arrayList;
-   // private List<String> timeListString;
     private List list;
     static String CLASSES = "classes";
     static String DATE = "date";
     static  String TIME = "time";
     static String INSTRUCTOR = "instructor";
     HashMap<String, String> map = new HashMap<String, String>();
+    private ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,47 +72,46 @@ public class MainActivity extends Activity {
         }
 
         @Override
-        protected Boolean doInBackground(Void... params) {
-            Log.d("where", "exect3");
+        protected Boolean doInBackground(Void... voids) {
             try {
                 Document doc = Jsoup.connect(PING_URL).get();
-                for(Element table : doc.select("table[class=schedule2]")){
-                    Elements content = table.select("tr");
-                    Element rowDays = content.select("tr").first();
-                    Elements days = rowDays.select("th");
-                    Element allClasses = content.get(1);
-                    Elements columns = allClasses.select("td");
-                    for(int i =1; i < columns.size(); i++){
-                        Element singleClass = columns.get(i);
+                for (Element table : doc.select("table[class=schedule2]")) {
+                    Elements rows = table.select("tr");
+                    Element columnsDays = rows.select("tr").first();
+                    Elements day = columnsDays.select("th");
+                    Element row = rows.get(1);
+                    Elements columns = row.select("td");
+                    for (int i = 1; i < columns.size(); i++) {
+                        Element column = columns.get(i);
                         List<String> timeListString = new ArrayList<String>();
-                        for(Element event : singleClass.select("div[class=inner]")){
-                            Elements hour = event.getElementsByClass("czas");
-                            timeListString.add(hour.text());
+                        for (Element event : column.select("div[class=inner]")) {
+                            Elements time = event.getElementsByClass("czas");
+                            timeListString.add(time.text());
                         }
                         Collections.sort(timeListString, new Comparator<String>() {
                             @Override
-                            public int compare(String hourFirst, String hourNext) {
+                            public int compare(String s, String t1) {
                                 try {
-                                    Matcher matcherToHourFirst = Pattern.compile("\\d+").matcher(hourFirst);
-                                    matcherToHourFirst.find();
-                                    Integer fistHour = Integer.valueOf(matcherToHourFirst.group());
-                                    Matcher matcherToHourNext = Pattern.compile("\\d+").matcher(hourNext);
-                                    matcherToHourNext.find();
-                                    Integer nextHour = Integer.valueOf(matcherToHourNext.group());
-                                    return fistHour.compareTo(nextHour);
+                                    Matcher matcher = Pattern.compile("\\d+").matcher(s);
+                                    matcher.find();
+                                    Integer integer1 = Integer.valueOf(matcher.group());
+                                    Matcher matcher2 = Pattern.compile("\\d+").matcher(t1);
+                                    matcher2.find();
+                                    Integer integer2 = Integer.valueOf(matcher2.group());
+                                    return integer1.compareTo(integer2);
                                 } catch (java.lang.NumberFormatException e) {
-                                    return hourFirst.compareTo(hourNext);
+                                    return s.compareTo(t1);
                                 }
                             }
                         });
-                        for(int j =0; j<timeListString.size();j++) {
-                            for (Element event : singleClass.select("div[class=inner]")) {
-                                Elements className = event.getElementsByClass("zajecia");
+                        for (int y = 0; y < timeListString.size(); y++) {
+                            Log.d("y2", timeListString.get(y));
+                            for (Element event : column.select("div[class=inner]")) {
+                                Elements name = event.getElementsByClass("zajecia");
                                 Elements instructor = event.getElementsByTag("a");
                                 Elements time = event.getElementsByClass("czas");
-                                if (time.equals(timeListString.get(j))) {
-                                    dbHandler.createTableRow(new Record(className.text(), instructor.text(), days.get(j).text(), time.text()));
-                                    Log.d("database", String.valueOf(dbHandler.getSortedRecords()));
+                                if (time.text().equals(timeListString.get(y))) {
+                                    dbHandler.createTableRow(new Record(time.text(), day.get(i).text(), name.text(), instructor.text()));
                                 }
                             }
                         }
@@ -127,28 +126,33 @@ public class MainActivity extends Activity {
 
         @Override
         protected void onPostExecute(Boolean result) {
-            super.onPostExecute(result);
             list = dbHandler.getSortedRecords();
             if(result == false || list.isEmpty()){
-                Toast.makeText(getApplicationContext(), "Przepraszamy wyst\u0105pi\u0142 problem z pobieraniem danych, " +
-                        " prosz\u0119 sprawdzi\u0107 grafik na stronie internetowej", Toast.LENGTH_LONG).show();
+                Toast toast= Toast.makeText(getApplicationContext(),
+                        "Przepraszamy wyst\u0105pi\u0142 problem z pobieraniem danych, " +
+                                " prosz\u0119 sprawdzi\u0107 grafik na stronie internetowej",
+                        Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER,0,0);
+                toast.show();
             }
-            else{
-            for(int i=0; i<list.size(); i++) {
-                record = (Record) list.get(i);
-                map.put("time", record.getTime());
-                map.put("instructor", record.getInstructor());
-                map.put("classes", record.getClasses());
-                map.put("date", record.getDate());
-                arrayList.add(map);
-                listView = (ListView) findViewById(R.id.listView);
-                adapter = new ListViewAdapter(MainActivity.this, arrayList);
-                listView.setAdapter(adapter);
-                Log.d("where", "exect4");
-            }
+            else {
+                for (int i = 0; i < list.size(); i++) {
+                    record = (Record) list.get(i);
+                    Log.d("record1", String.valueOf(record));
+                    HashMap<String, String> map = new HashMap<String, String>();
+                    map.put("time", record.getTime());
+                    map.put("date", record.getDate());
+                    map.put("classes", record.getClasses());
+                    map.put("instructor", record.getInstructor());
+                    arrayList.add(map);
+                    listview = (ListView) findViewById(R.id.listView);
+                    adapter = new ListViewAdapter(MainActivity.this, arrayList);
+                    listview.setAdapter(adapter);
+                }
             }
             progressDialog.dismiss();
         }
+
     }
 
     @Override
